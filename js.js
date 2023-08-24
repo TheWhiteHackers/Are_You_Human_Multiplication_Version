3e5cfb7e251caa023024c6aac402d82147908b5d
TheWhiteHackers <nurefsanbera@gmail.com>
2023-08-23 20:25:03 -0500

    popups made....

---
+++ js.js
@@ -0,0 +1,15 @@
+window.onload=start();
+
+function start(){
+    document.getElementById("shadow").style.display="none";
+    document.getElementById("explainpopup").style.display="none";
+}
+
+function closebtn(){
+    document.getElementById("shadow").style.display="none";
+    document.getElementById("explainpopup").style.display="none";
+}
+function explain() {
+    document.getElementById("shadow").style.display="block";
+    document.getElementById("explainpopup").style.display="block";
+}
