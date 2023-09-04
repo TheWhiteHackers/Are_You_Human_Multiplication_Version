912c3f3ee21c610fb073c1e52ac58c2b37320bcf
TheWhiteHackers <nurefsanbera@gmail.com>
2023-09-04 14:58:29 -0500

    numbers....

--- js.js
+++ js.js
@@ -1,10 +1,14 @@
+let correct = 0;
+let incorrect = 0;
+let qanswered = 0;
+let diff = 4;
+
 window.onload=start();
 
 function start(){
     document.getElementById("shadow").style.display="none";
     document.getElementById("explainpopup").style.display="none";
 }
-
 function closebtn(){
     document.getElementById("shadow").style.display="none";
     document.getElementById("explainpopup").style.display="none";
@@ -16,4 +20,13 @@ function explain() {
 function begin(){
     document.getElementById("startbox").style.display="none";
     
+    let num1 = Math.floor(Math.random() * diff );
+    let num2 = Math.floor(Math.random() * diff );
+    let text = num1 + " X " + num2 + " =";
+    let theans = num1*num2;
+
+
+    console.log(num1, num2, theans);
+    
+    document.getElementById("equ").innerHTML=text;
 }    
